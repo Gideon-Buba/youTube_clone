a387843f2ff1cd78f51c2a0702bdce90b4e3196d
Gideon Buba <user@users-MacBook-Pro.local>
2023-11-09 11:22:03 +0100

    Added search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Box } from "@mui/material"
 
-import { NavBar, Feed, VideoDetail, ChannelDetail ,SearchFeed } from "./components" 
+import { NavBar, Feed, VideoDetail, ChannelDetail ,SearchFeed } from "./components"  
 
 const App = () =>  (
     <BrowserRouter>
-        <Box sx={{backgroundColor: '#0000'}} >
+        <Box sx={{backgroundColor: '#000'}} >
             < NavBar/>
             <Routes>
                 <Route path="/" exact element={<Feed />}/>
